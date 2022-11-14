c08895e5a4484aa6e4d4a22e2dbcf209d71748b1
willgsw22 <willbrad2323@gmail.com>
2022-11-13 21:10:29 -0600

    fixed the errors

--- myHeroApp/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ myHeroApp/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HEROES } from '../Mock-Heroes';
 
 @Component({
   selector: 'app-heroes',
