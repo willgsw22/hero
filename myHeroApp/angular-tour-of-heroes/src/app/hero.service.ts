0bbed3fa8df59b6278b4f2bc85460493035d2492
willgsw22 <willbrad2323@gmail.com>
2022-11-18 18:53:11 -0600

    now send a message when the hero is fetched

--- myHeroApp/angular-tour-of-heroes/src/app/hero.service.ts
+++ myHeroApp/angular-tour-of-heroes/src/app/hero.service.ts
@@ -9,6 +9,7 @@ import { MessageService } from './message.service';
 export class HeroService {
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
   constructor(private messageService: MessageService) { }
