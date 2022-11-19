977944e1f36901abe62ed6540e9ce42e807f0f18
willgsw22 <willbrad2323@gmail.com>
2022-11-18 18:49:38 -0600

    now the message service is private

--- myHeroApp/angular-tour-of-heroes/src/app/hero.service.ts
+++ myHeroApp/angular-tour-of-heroes/src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './Mock-Heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,5 +11,5 @@ export class HeroService {
     const heroes = of(HEROES);
     return heroes;
   }
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
