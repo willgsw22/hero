93523f69d55c472a11fbb0c05fa421edf871f458
willgsw22 <willbrad2323@gmail.com>
2022-12-02 17:38:56 -0600

    created a redirectory

--- myHeroApp/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ myHeroApp/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
