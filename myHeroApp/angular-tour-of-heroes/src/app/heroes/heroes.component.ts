b4d926d40ac8d34dd47dcdeda20b3618a65b161b
willgsw22 <willbrad2323@gmail.com>
2022-12-02 18:03:51 -0600

    cleaned up some code

--- myHeroApp/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ myHeroApp/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,28 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-
-@Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
-})
-
 export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
 
-  heroes: Hero[] = [];;
-  selectedHero?: Hero;
+  constructor(private heroService: HeroService) { }
 
-  constructor(private heroService: HeroService) {}
-  getHeroes(): void {
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-  }
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
